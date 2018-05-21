52b4d2ef82291288e6b689cd19261a0cdacc6965
luishens01 <38247377+luishens01@users.noreply.github.com>
2018-05-21 15:31:53 +0200

    change in main script
    change in main script

--- main_script.sh
+++ main_script.sh
@@ -6,7 +6,8 @@ echo "This is the environment file you have select:"
 echo $ENV
 
 cd components/packages/
-sh test_script.sh ''$ENV''
+VAR=$(sh test_script.sh ''$ENV'')
+echo $VAR
 
 echo "Main script finished"
 
