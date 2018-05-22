004a2195a089add1cd9be83061efcb5c22d8a1b5
luishens01 <luis.hensruiz.external@atos.net>
2018-05-22 12:03:35 +0200

    updating ts

--- components/packages/test_script.sh
+++ components/packages/test_script.sh
@@ -22,7 +22,7 @@ echo
 echo
 result_delete=$(tavern-ci delete_packages.yml --stdout --debug)
 echo $result_delete
-echo "" > ../../results/packages/delete_package_results.log
+echo "" > ../../results/packages/delete_packages_results.log
 echo $result_delete >> ../../results/packages/delete_packages_results.log
 echo
 sed -i -- "s/$ENV/environment_file/g" delete_packages.yml
@@ -50,7 +50,7 @@ echo
 echo
 result_get=$(tavern-ci get_packages.yml --stdout --debug)
 echo $result_get
-echo "" > ../../results/packages/get_package_results.log
+echo "" > ../../results/packages/get_packages_results.log
 echo $result_get >> ../../results/packages/get_packages_results.log
 echo
 sed -i -- "s/$ENV/environment_file/g" get_packages.yml
